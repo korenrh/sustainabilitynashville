11e8c38f4147684a1980f8a85a5498ae4e1f27a1
Reilly Koren <49700126+korenrh@users.noreply.github.com>
2021-12-02 10:47:55 -0600

    rough carousel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { Carousel, CarouselItem } from "react-bootstrap";
+import "../css/home.css"
+
 
 function Home() {
   return (
     <div className="Home">
       <main>
-        <h1>Home</h1>
+        <Carousel className = "carousel-container">
+          <CarouselItem>
+            <img
+            className = "carousel-image"
+            src = "/stateCapital.jpg" 
+            />
+            <Carousel.Caption>
+            <a href="https://arcg.is/1aTu4X"><button type="button" class="carousel-button">Raise a Sustainability Concern</button></a>
+            </Carousel.Caption>
+          </CarouselItem>
+          <CarouselItem>
+            <img
+            className = "carousel-image"
+            src = "/NashvilleMap.jpg"
+            />
+            <Carousel.Caption>
+            <a href="https://arcg.is/1aTu4X"><button type="button" class="carousel-button">Explore Nashville's Sustainability Map</button></a>
+            </Carousel.Caption>
+          </CarouselItem>
+        </Carousel>
       </main>
     </div>
   );
